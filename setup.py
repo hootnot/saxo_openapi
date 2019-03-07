bbbb648fbedbc3154029901ca547a1b4440cc511
hootnot <f.brekeveld@gmail.com>
2019-03-07 10:29:10 +0100

    packaging config update [ci skip]

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     include_package_data=True,
     keywords='saxo_openapi',
     name='saxo_openapi',
-    packages=find_packages(include=['saxo_openapi']),
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
