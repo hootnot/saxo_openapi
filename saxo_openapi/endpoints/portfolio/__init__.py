84ad70e3c6a68aa22e5f7fd7ff38dce6570d385b
hootnot <f.brekeveld@gmail.com>
2019-03-27 15:03:17 +0100

    restructure portfolio subpackage

---
+++ saxo_openapi/endpoints/portfolio/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from . import (    # noqa: F401
+    accountgroups,
+    accounts,
+    balances,
+    clients,
+    closedpositions,
+    exposure,
+    netpositions,
+    orders,
+    positions,
+    users)
