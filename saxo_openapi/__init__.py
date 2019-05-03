b708ad5df28ac126138ea14349f82cda6b788bbd
hootnot <f.brekeveld@gmail.com>
2019-05-04 00:55:31 +0200

    force 0.2.0

--- saxo_openapi/__init__.py
+++ saxo_openapi/__init__.py
@@ -5,7 +5,7 @@
 __title__ = "Saxo OpenAPI API Wrapper"
 __author__ = """Feite Brekeveld"""
 __email__ = 'f.brekeveld@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __copyright__ = "Copyright 2019 Feite Brekeveld"
 
 
