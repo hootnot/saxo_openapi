29aa23f5b8c2d1783fc0c298beeb82e1d5c9ded4
hootnot <f.brekeveld@gmail.com>
2019-05-22 00:36:56 +0200

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hootnot/saxo_openapi',
-    version='0.3.1',
+    version='0.4.0',
     zip_safe=False,
 )
