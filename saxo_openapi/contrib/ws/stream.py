b7297a90dd6a9b2d664ec5c46c04e72c7d682bf1
hootnot <f.brekeveld@gmail.com>
2021-02-23 10:29:19 +0100

    fix(streaming quotes): fixed to handle multiple messages bundled in 1 websocket message
    BREAKING CHANGE: decode_ws_msg() is now a generator returning 1 or more
    decoded messages. See documentation for details.

--- saxo_openapi/contrib/ws/stream.py
+++ saxo_openapi/contrib/ws/stream.py
@@ -20,25 +20,29 @@ def decode_ws_msg(raw):
     The value assigned to msg can be parsed JSON data, so a dict, or
     a string.
     """
-    (msgIdentifier,), raw = struct.unpack_from("Q", raw[:8]), raw[8+2:]
-    # (Reserved,), raw = struct.unpack_from("h", raw[:2]), raw[2:]
+    while raw:
+        (msgIdentifier,), raw = struct.unpack_from("Q", raw[:8]), raw[8+2:]
+        # (Reserved,), raw = struct.unpack_from("h", raw[:2]), raw[2:]
 
-    (Srefid,), raw = struct.unpack_from("B", raw[:1]), raw[1:]
-    FS = "{}s".format(Srefid)
-    (refid,), raw = struct.unpack_from(FS, raw[:Srefid]), raw[Srefid:]
-    (payloadFmt,), raw = struct.unpack_from("B", raw[:1]), raw[1:]
-    (payloadSize,), raw = struct.unpack_from("i", raw[:4]), raw[4:]
+        (Srefid,), raw = struct.unpack_from("B", raw[:1]), raw[1:]
+        FS = "{}s".format(Srefid)
+        (refid,), raw = struct.unpack_from(FS, raw[:Srefid]), raw[Srefid:]
+        (payloadFmt,), raw = struct.unpack_from("B", raw[:1]), raw[1:]
+        (payloadSize,), raw = struct.unpack_from("i", raw[:4]), raw[4:]
 
-    FS = "{}s".format(payloadSize)
-    (payload,) = struct.unpack_from(FS, raw[:payloadSize])
+        FS = "{}s".format(payloadSize)
+        (payload,) = struct.unpack_from(FS, raw[:payloadSize])
 
-    msg = {'refid': refid,
-           'msgId': msgIdentifier}
+        msg = {'refid': str(refid, 'utf-8'),
+               'msgId': msgIdentifier}
 
-    if payloadFmt == 0:
-        msg.update({'msg': json.loads(str(payload, 'utf-8'))})
+        if payloadFmt == 0:
+            msg.update({'msg': json.loads(str(payload, 'utf-8'))})
+            # msg.update({'msg': json.loads(str(payload).decode('utf-8'))})
 
-    else:
-        msg.update({'msg': payload})
+        else:
+            msg.update({'msg': payload})
 
-    return msg
+        raw = raw[payloadSize:]
+
+        yield msg
--- tests/test_contrib_ws.py
+++ tests/test_contrib_ws.py
@@ -29,3 +29,120 @@ class TestContribWS(unittest.TestCase):
                }
         decmsg = wsstr.decode_ws_msg(raw)
         self.assertTrue(decmsg, res)
+
+    def test_stream_decode_ws_msg1(self):
+        raw = b'\x8b\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'AUDJPY\x00\\\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.971000Z","Quote":{"Ask":83.27,"Bid":83.22,"Mid":83.245}}' \
+            + b'\x8c\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'GBPCAD\x00a\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.976000Z","Quote":{"Ask":1.77132,"Bid":1.77052,"Mid":1.77092}}' \
+            + b'\x8d\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'GBPNZD\x00a\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.979000Z","Quote":{"Ask":1.91672,"Bid":1.91552,"Mid":1.91612}}' \
+            + b'\x8e\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'NZDCHF\x00a\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.979000Z","Quote":{"Ask":0.65704,"Bid":0.65634,"Mid":0.65669}}' \
+            + b'\x8f\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'GBPUSD\x00a\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.976000Z","Quote":{"Ask":1.40218,"Bid":1.40188,"Mid":1.40203}}' \
+            + b'\x90\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'NZDCAD\x00a\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.979000Z","Quote":{"Ask":0.92462,"Bid":0.92382,"Mid":0.92422}}' \
+            + b'\x91\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'NZDUSD\x00^\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.979000Z","Quote":{"Ask":0.7319,"Bid":0.7315,"Mid":0.7317}}' \
+            + b'\x92\x03\x00\x00\x00\x00\x00\x00\x00\x00\x06' \
+            + b'GBPAUD\x00a\x00\x00\x00{"LastUpdated":"2021-02-22T12:45:52.976000Z","Quote":{"Ask":1.77906,"Bid":1.77816,"Mid":1.77861}}'  # noqa E501
+
+        res = [{
+                "refid": "AUDJPY",
+                "msgId": 907,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.971000Z",
+                  "Quote": {
+                    "Ask": 83.27,
+                    "Bid": 83.22,
+                    "Mid": 83.245
+                  }
+                }
+              },
+              {
+                "refid": "GBPCAD",
+                "msgId": 908,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.976000Z",
+                  "Quote": {
+                    "Ask": 1.77132,
+                    "Bid": 1.77052,
+                    "Mid": 1.77092
+                  }
+                }
+              },
+              {
+                "refid": "GBPNZD",
+                "msgId": 909,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.979000Z",
+                  "Quote": {
+                    "Ask": 1.91672,
+                    "Bid": 1.91552,
+                    "Mid": 1.91612
+                  }
+                }
+              },
+              {
+                "refid": "NZDCHF",
+                "msgId": 910,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.979000Z",
+                  "Quote": {
+                    "Ask": 0.65704,
+                    "Bid": 0.65634,
+                    "Mid": 0.65669
+                  }
+                }
+              },
+              {
+                "refid": "GBPUSD",
+                "msgId": 911,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.976000Z",
+                  "Quote": {
+                    "Ask": 1.40218,
+                    "Bid": 1.40188,
+                    "Mid": 1.40203
+                  }
+                }
+              },
+              {
+                "refid": "NZDCAD",
+                "msgId": 912,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.979000Z",
+                  "Quote": {
+                    "Ask": 0.92462,
+                    "Bid": 0.92382,
+                    "Mid": 0.92422
+                  }
+                }
+              },
+              {
+                "refid": "NZDUSD",
+                "msgId": 913,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.979000Z",
+                  "Quote": {
+                    "Ask": 0.7319,
+                    "Bid": 0.7315,
+                    "Mid": 0.7317
+                  }
+                }
+              },
+              {
+                "refid": "GBPAUD",
+                "msgId": 914,
+                "msg": {
+                  "LastUpdated": "2021-02-22T12:45:52.976000Z",
+                  "Quote": {
+                    "Ask": 1.77906,
+                    "Bid": 1.77816,
+                    "Mid": 1.77861
+                  }
+                }
+              }]
+        decmsgs = [m for m in wsstr.decode_ws_msg(raw)]
+        self.assertTrue(decmsgs, res)
