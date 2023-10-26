6014bfdb3f8043e90356323d457b0f6136935e4f
Oliver Polden <oliver@hoverfly.media>
2023-10-26 12:15:32 +0100

    Update comments from currencies to currencypairs.

--- saxo_openapi/endpoints/referencedata/currencypairs.py
+++ saxo_openapi/endpoints/referencedata/currencypairs.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-"""Handle referencedata-currencies endpoints."""
+"""Handle referencedata-currencypairs endpoints."""
 
 from ..decorators import dyndoc_insert, endpoint
 from .base import ReferenceData
@@ -9,17 +9,17 @@ from .responses.currencies import responses
 
 @endpoint("openapi/ref/v1/currencypairs/")
 class CurrencyPairs(ReferenceData):
-    """Get a list all supported currencies."""
+    """Get data on currency pairs."""
 
     @dyndoc_insert(responses)
     def __init__(self):
-        """Instantiate a Currencies request.
+        """Instantiate a Currency Pairs request.
 
         >>> import json
         >>> import saxo_openapi
         >>> import saxo_openapi.endpoints.referencedata as rd
         >>> client = saxo_openapi.API(access_token=...)
-        >>> r = rd.currencies.Currencies()
+        >>> r = rd.currencies.CurrencyPairs()
         >>> client.request(r)
         >>> print(json.dumps(r.response, indent=4))
 
